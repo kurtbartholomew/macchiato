440df9250c74c81d8ddccf90178cd2e24f88e5e4
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2018-10-17 08:04:30 -0500

    (test) Added tests for source

--- src/test/java/frontend/SourceTest.java
+++ src/test/java/frontend/SourceTest.java
@@ -17,6 +17,7 @@ public class SourceTest {
     public static final String TEST_FILE = "testFile.txt";
 
     BufferedReader reader;
+    Source source;
 
     @Rule
     public final ExternalResource resource = new ExternalResource() {
@@ -24,6 +25,7 @@ public class SourceTest {
         protected void before() throws Throwable {
             Path filePath = Paths.get("src","test","resources",TEST_FILE);
             reader = new BufferedReader(new FileReader(filePath.toFile()));
+            source = new Source(reader);
         }
 
         @Override
@@ -37,38 +39,86 @@ public class SourceTest {
     @Test
     public void currentCharShouldReturnTheFirstCharacterInFile() {
 
-    }
-
-    @Test
-    public void currentCharShouldReturnEndOfFile() {
-
+        char c = ' ';
+        try {
+            c = source.currentChar();
+        } catch(IOException e) { }
+        assertEquals('T', c);
     }
 
     @Test
     public void currentCharShouldReturnEndOfLine() {
+        char c = ' ';
+        try {
+            source.currentChar();
+            source.nextChar();
+            c = source.currentChar();
+        } catch(IOException e) { }
+        assertEquals(source.EOL, c);
 
+    }
+
+    @Test
+    public void currentCharShouldReturnEndOfFile() {
+        char c = ' ';
+        try {
+            source.currentChar();
+            source.nextChar();
+            source.nextChar();
+            source.nextChar();
+            source.nextChar();
+            c = source.currentChar();
+        } catch(IOException e) { }
+        assertEquals(source.EOF, c);
     }
 
     @Test
     public void currentCharShouldReturnCharacterAtStartOfNextLine() {
-
+        char c = ' ';
+        try {
+            source.currentChar();
+            source.nextChar();
+            source.nextChar();
+            c = source.currentChar();
+        } catch(IOException e) { }
+        assertEquals('B', c);
     }
 
     @Test
-    public void currentCharShouldReturnCurrentCharacter() {
-
-    }
-
-
-    @Test
-    public void nextChar() {
-    }
-
-    @Test
-    public void peekChar() {
+    public void nextCharReturnsNextCharacter() {
+        char c = ' ';
+        try {
+            source.currentChar();
+            c = source.nextChar();
+        } catch(IOException e) {}
+        assertEquals(source.EOL, c);
     }
 
     @Test
-    public void close() {
+    public void peekCharShouldReturnTheNextCharacterWithoutChangingCurrent() {
+        char n = ' ';
+        char c = ' ';
+        try {
+            source.currentChar();
+            n = source.peekChar();
+            c = source.currentChar();
+        } catch(IOException e) {}
+        assertEquals(source.EOL, n);
+        assertEquals('T', c);
+    }
+
+    @Test
+    public void peekCharShouldReturnEOFIfAtEndOfFile() {
+        char n = ' ';
+        try {
+            source.currentChar();
+            source.nextChar();
+            source.nextChar();
+            source.nextChar();
+            source.nextChar();
+            n = source.peekChar();
+        } catch (IOException e) {
+        }
+        assertEquals(source.EOF, n);
     }
 }
