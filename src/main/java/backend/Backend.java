64dbc59088bc601d555ad9783948a64f0c76ad79
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2018-10-23 21:02:32 -0500

    (feat) Added backend stubs

--- src/main/java/backend/Backend.java
+++ src/main/java/backend/Backend.java
@@ -1,4 +1,14 @@
 package backend;
 
-public class Backend {
+import message.*;
+
+import java.io.IOException;
+
+public abstract class Backend implements MessageProducer {
+    protected static MessageHandler messageHandler = new MessageHandler();
+
+    protected SymTab symTab;
+    protected ICode iCode;
+
+    public abstract void process(ICode iCode, SymTab symTab) throws IOException;
 }
